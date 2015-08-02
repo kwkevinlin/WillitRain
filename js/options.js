7648dec0250374ba5b7676f129b376a6d500999f
Kevin Lin <kevin.lin@timeinc.com>
2015-08-01 23:01:53 -0400

    Added alarms and notification to Manifest, and updated Options page

--- js/options.js
+++ js/options.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
     //Currently stored cityName -> value attribute
     var cityName = localStorage.cityName;
     $('#cityOptionInput').attr('value', cityName);
+
+    //Currently stored checkFreq -> value attribute
+    var checkFreq = localStorage.checkFreq;
+    $('#checkFreqInput').attr('value', checkFreq);
+
 });
 
 $('#save-options-button').on('click', function() {
@@ -10,6 +15,18 @@ $('#save-options-button').on('click', function() {
     var cityName = $('#cityOptionInput').val();
     localStorage.cityName = cityName;
     console.log("Local Storage 'cityName': " + cityName);
+
+    var checkFreq = $('#checkFreqInput').val();
+    if(checkFreq >= 3) {
+        $('#checkFreqError').hide();
+        localStorage.checkFreq = checkFreq;
+        console.log("Local Storage 'cityName': " + checkFreq);
+
+        $('#optionsSaved').show().delay(2000).fadeOut(2000);
+    } else 
+        $('#checkFreqError').show();
+
+
 
     //Checklist
     //Make a green checkbox appear after successful save
